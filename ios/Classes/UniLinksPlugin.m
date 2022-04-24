957b6e14d39f0976861437d9eb05386e50699e23
hassan alsabti <7snsabti@gmail.com>
2022-04-24 13:07:34 +0300

    IOS Reset

--- ios/Classes/UniLinksPlugin.m
+++ ios/Classes/UniLinksPlugin.m
@@ -78,7 +78,7 @@ static id _instance;
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
   if ([@"getInitialLink" isEqualToString:call.method]) {
     result(self.initialLink);
-    self.initialLink = "";
+    self.initialLink = @"";
     // } else if ([@"getLatestLink" isEqualToString:call.method]) {
     //     result(self.latestLink);
   } else {
